199b46cac25723358bda3c89d9ca348a5e86754b
JuseungPark <zhujack26@gmail.com>
2024-02-08 11:30:36 +0900

    Excercise 2-5 Few people

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
 
-export default class StudentBrowser extends LightningElement {}
+export default class StudentBrowser extends LightningElement {
+    studentList = [];
+    constructor() {
+        super();
+        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
+        this.studentList = studentNames.map( (studentName, index) => {
+          return {
+            'sobjectType': 'Contact',
+            'Name': studentName,
+            'PhotoUrl': '/services/images/photo/003B0FakePictId',
+            'Id': index
+          };
+        });
+      }
+}
---
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTiles extends LightningElement
+{
+    @api studentList = [];
+}
