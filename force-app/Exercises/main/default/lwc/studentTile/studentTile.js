3d5e943e540109bea939419f71469a52805785f9
JuseungPark <zhujack26@gmail.com>
2024-02-08 10:24:53 +0900

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,13 @@
+import { LightningElement , api} from 'lwc';
+
+export default class StudentTile extends LightningElement
+{
+    @api student = {
+        Name: '[Juseung Park]',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+        };
+    @api isSelected = false;
+    get tileSelected() {
+        return this.isSelected ? "tile selected" : "tile";
+        }
+}
