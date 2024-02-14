b8b2665709165d662aeb761fde3920085fbf9d00
JuseungPark <zhujack26@gmail.com>
2024-02-14 10:08:54 +0900

    Exercise 2-9 Contact Selected

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,19 +1,22 @@
-import { LightningElement , api} from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class StudentTile extends LightningElement
-{
-    @api student = {
-        Name: '[Juseung Park]',
-        PhotoUrl: '/services/images/photo/003B0FakePictId',
-        };
-    @api isSelected = false;
-    get tileSelected() {
-        return this.isSelected ? "tile selected" : "tile";
-        }
-    studentClick(){
-        alert(this.student.Name);
-        /* debugger;
+export default class StudentTile extends LightningElement {
+	@api student = {
+		Name: "[Juseung Park]",
+		PhotoUrl: "/services/images/photo/003B0FakePictId"
+	};
+	get tileSelected() {
+		return this.selectedStudentId === this.student.Id ? "tile selected" : "tile";
+	}
+	studentClick() {
+		// alert(this.student.Name);
+		/* debugger;
         console.log('Name', this.student.Name);
         console.log('Photo', this.student.PhotoUrl); */
-    }
+		const evt = new CustomEvent("studentselected", {
+			detail: { studentId: this.student.Id }
+		});
+		this.dispatchEvent(evt);
+	}
+	@api selectedStudentId = "";
 }
--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -1,6 +1,9 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
-export default class StudentTiles extends LightningElement
-{
-    @api studentList = [];
+export default class StudentTiles extends LightningElement {
+	@api studentList = [];
+	selectedStudentId = "";
+	handleStudentSelected(event) {
+		this.selectedStudentId = event.detail.studentId;
+	}
 }
