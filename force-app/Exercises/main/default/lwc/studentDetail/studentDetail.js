f4b2b1bb19a0348db3d57c67fdeb76b1ab71b7a2
JuseungPark <zhujack26@gmail.com>
2024-02-15 15:33:02 +0900

    change branch name

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -17,8 +17,6 @@ import SELECTED_STUDENT_CHANNEL from "@salesforce/messageChannel/SelectedStudent
 const fields = [FIELD_Name, FIELD_Description, FIELD_Email, FIELD_Phone];
 export default class StudentDetail extends LightningElement {
 	// TODO #3: locate a valid Contact ID in your scratch org and store it in the studentId property.
-	// Example: studentId = '003S000001SBAXEIA5';
-	//studentId = "0037i00001DoDkEAAV";
 
 	//TODO #4: use wire service to call getRecord, passing in our studentId and array of fields.
 	//		   Store the result in a property named wiredStudent.
