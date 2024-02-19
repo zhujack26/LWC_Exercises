98e478fe344b58daa841d1abb1ce9a82c26e59c8
JuseungPark <zhujack26@gmail.com>
2024-02-19 10:36:27 +0900

    Exercise 4-3 Completed

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, wire } from "lwc";
 import getCertifiedStudents from "@salesforce/apex/CertifiedStudentList.getCertifiedStudents";
+import deleteStudentCertification from "@salesforce/apex/CertifiedStudentList.deleteStudentCertification";
+import { refreshApex } from "@salesforce/apex";
 
 export default class CertifiedStudentList extends LightningElement {
 	@api certificationId = 0;
@@ -7,8 +9,10 @@ export default class CertifiedStudentList extends LightningElement {
 	certifiedStudents;
 	btnGropupDisabled = true;
 	Error;
+	_wiredStudentResult;
 	@wire(getCertifiedStudents, { certificationId: "$certificationId" })
 	wired_getCertifiedStudents(result) {
+		this._wiredStudentResult = result;
 		this.certifiedStudents = [];
 		if (result.data) {
 			this.certifiedStudents = result.data.map((certHeld) => ({
@@ -26,6 +30,35 @@ export default class CertifiedStudentList extends LightningElement {
 	onRowSelection(event) {
 		const numSelected = event.detail.selectedRows.length;
 		this.btnGroupDisabled = numSelected === 0;
+	}
+	getSelectedIDs() {
+		const datatable = this.template.querySelector("lightning-datatable");
+		const ids = datatable.getSelectedRows().map( (r) => (r.certificationHeldId));
+		return ids;
+	}
+	onCertAction(event) {
+		const btnClicked = event.target.getAttribute("data-btn-id");
+		switch (btnClicked) {
+			case "btnEmail":
+				break;
+			case "btnSendCert":
+				break;
+			case "btnDelete":
+				this.onDelete();
+				break;
+			default:
+				break;
+		}
+	}
+	onDelete() {
+		const certificationIds = this.getSelectedIDs();
+		deleteStudentCertification({ certificationIds })
+			.then(() => {
+				refreshApex(this._wiredStudentResult);
+			})
+			.catch((error) => {
+				this.error = error;
+			});
 	}
 	columnConfig = [
 		{
