8486cf254c73093773f84a6b410a4d377e29eb77
JuseungPark <zhujack26@gmail.com>
2024-02-19 15:55:09 +0900

    fix

--- force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
+++ force-app/Exercises/main/default/lwc/studentTiles/studentTiles.js
@@ -6,6 +6,13 @@ export default class StudentTiles extends LightningElement {
 	@api setSelectedStudent(studentId) {
 		this.selectedStudentId = studentId;
 	}
+	@api setSelectedRecord(recordId) {
+		const mySelector = `tr[data-pk='${recordId}']`;
+		const selectedRow = this.template.querySelector(mySelector);
+		if (selectedRow) {
+			this.highlightSelectedRow(selectedRow);
+		}
+	}
 	handleStudentSelected(event) {
 		this.selectedStudentId = event.detail.studentId;
 	}
