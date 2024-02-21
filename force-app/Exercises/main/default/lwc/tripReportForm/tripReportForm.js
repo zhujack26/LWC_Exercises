ba8884ecd9ab885cc2a8d432abf897aa8a24b2b1
JuseungPark <zhujack26@gmail.com>
2024-02-21 10:05:34 +0900

    Exercise 5-6 and 6-1 Completed

---
+++ force-app/Exercises/main/default/lwc/tripReportForm/tripReportForm.js
@@ -0,0 +1,8 @@
+import { LightningElement } from "lwc";
+import Utils from "c/utils";
+
+export default class TripReportForm extends LightningElement {
+	doSuccess() {
+		Utils.showToast(this, "Transaction Complete", "Your Trip Report was Saved");
+	}
+}
